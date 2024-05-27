2b86f1b03409247159a6fe125065a3cbc99087ba
aditya <aditdualapan@gmail.com>
2024-05-27 11:13:11 +0700

    wip footer

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Container from "@/components/container";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar></Navbar>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
---
+++ components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import Container from "../container";
+
+export default function Footer() {
+  return (
+    <footer>
+      <Container>
+        <div className="py-12">Halo</div>
+      </Container>
+    </footer>
+  );
+}
